074340f238db79d6ff0a75f94b53c0ad109f1851
MathiasRekyan <79878805+MathiasRekyan@users.noreply.github.com>
2021-05-09 07:36:45 +0700

    Update ping.js

--- commands/normal/ping.js
+++ commands/normal/ping.js
@@ -5,14 +5,11 @@ module.exports.run = async (Client, message, args, prefix) => { // for the cmd h
 
     if(!message.content.startsWith(prefix)) return; // makes sure it starts with the prefix
 
-    const ping = new Discord.MessageEmbed()
-        .setDescription(`🏓\`${client.ws.ping}\`ms`);
-
-
-        message.channel.send(ping);
-    }
-
-
+    message.channel.send(`Finding the bot ping...`).then(msg => { // sends this once you send the cmd
+        const ping = msg.createdTimestamp - message.createdTimestamp; // calculation the time between when u send the message and when the bot reply
+        msg.edit(`🏓\` ${ping}\`ms!`) // it will edit the msg to this after it gets the ping!
+    })
+}
 
 module.exports.help = {
     name: "ping", // name of the cmd
