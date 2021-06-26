a103d54dd0ae8aa9006f0fff8d3f077e4beb4045
MathiasRekyan <79878805+MathiasRekyan@users.noreply.github.com>
2021-06-26 21:58:27 +0700

    Update waltz.js

--- commands/weapon bow/waltz.js
+++ commands/weapon bow/waltz.js
@@ -7,7 +7,7 @@ module.exports.run = async (Client, message, args, prefix) => {
     .setTitle("Mitternachts Waltz")
     .addField("\u200B","⧫ Type: " + "Bow" + "\r\n ⧫ Rarity: " + "⭐⭐⭐⭐" + "\r\n ⧫ Location: " + "Gacha" , true)
     .addField("\u200B", "⧫ ATK: " + "42 (max 510)" + "\r\n ⧫ 2nd Stat: " + "Physical DMG bonus 11.3% (Maks 51.9%)", true)
-    .addField("Passive Ability: Infusion Arrow", "Normal Attack and Charged Attack hits increases ATK by 4-8% and Normal Attack SPD by 1.2-2.4 for 6s. Max 4 stacks. Can only occur once every 0.3s.")
+    .addField("Passive Ability: Infusion Arrow", "Normal Attack hits on opponents increase Elemental Skill DMG by 20% for 5s. Elemental Skill hits on opponents increase Normal Attack DMG by 20% for 5s.")
     .setImage("https://static.wikia.nocookie.net/gensin-impact/images/7/77/Weapon_Mitternachts_Waltz.png/revision/latest/scale-to-width-down/256?cb=20210611013556", 2, 2) 
     .setThumbnail("https://static.wikia.nocookie.net/gensin-impact/images/8/81/Icon_Bow.png/revision/latest/scale-to-width-down/128?cb=20210413210801")
     .setFooter("Look for another? try ^list") ;
