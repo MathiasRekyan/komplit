08a2c04ed479a8d5d4d85ba110c4690b2a10dae6
MathiasRekyan <79878805+MathiasRekyan@users.noreply.github.com>
2021-07-28 18:34:31 +0700

    Update katsuragi.js

--- commands/weapon claymore/katsuragi.js
+++ commands/weapon claymore/katsuragi.js
@@ -5,7 +5,7 @@ const Discord = require('discord.js');
         
     const embed = new Discord.MessageEmbed()
     .setTitle("	Katsuragikiri Nagamasa")
-    .addField("\u200B", "⧫ Type: " + "Claymore" + "\r\n ⧫ Rarity: " + "⭐⭐⭐" + "\r\n ⧫ Location: " + "Gacha" , true) //change the "Weapon Name", "Rarity", and "Location"
+    .addField("\u200B", "⧫ Type: " + "Claymore" + "\r\n ⧫ Rarity: " + "⭐⭐⭐⭐" + "\r\n ⧫ Location: " + "Gacha" , true) //change the "Weapon Name", "Rarity", and "Location"
     .addField("\u200B", "⧫ ATK: " + "42 (max 510)" + "\r\n ⧫ 2nd Stat: " + "Energy Recharge+ 10% (max 45.9%%)", true) //Fill the ... (fill the 2nd stat with max lvl too)
     .addField("Passive Ability: Samurai Conduct", "Increases Elemental Skill DMG by 6~12%. After Elemental Skill hits an opponent, the character loses 3 Energy but regenerates 3~5 Energy every 2s for the next 6s. This effect can occur once every 10s. Can be triggered even when the character is not on the field.") //change the Skill Name, And Skill description (passive ability)
     .setImage("https://rerollcdn.com/GENSHIN/Weapon/NEW/Katsuragikiri_Nagamasa.png", 2, 2) //fill with image of the weapon if u want 
