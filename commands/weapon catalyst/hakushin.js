02c2dc2d4258b0639620f2921927a9065a8f5d66
MathiasRekyan <79878805+MathiasRekyan@users.noreply.github.com>
2021-07-28 18:39:43 +0700

    Create hakushin.js

---
+++ commands/weapon catalyst/hakushin.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (Client, message, args, prefix) => {
+    if(!message.content.startsWith(prefix)) return;
+
+    const embed = new Discord.MessageEmbed()
+    .setTitle("Hakushin Ring")
+    .addField("\u200B", "⧫ Type: " + "Catalyst" + "\r\n ⧫ Rarity: " + "⭐⭐⭐⭐" + "\r\n ⧫ Location: " + "Forging" , true)
+    .addField("\u200B", "⧫ ATK: " + "44 (max 565)" + "\r\n ⧫ 2nd Stat: " + "Energy Recharge+ 6.7% (max 30.6%)", true)
+    .addField("Passive Ability: Press the Advantage", "After defeating an opponent, ATK is increased by 12% for 30s. This effect has a maximum of 3 stacks, and the duration of each stack is independent of the others.")
+    .setImage("https://rerollcdn.com/GENSHIN/Weapon/NEW/Hakushin_Ring.png", 2, 2)
+    .setThumbnail("https://static.wikia.nocookie.net/gensin-impact/images/2/27/Icon_Catalyst.png/revision/latest/scale-to-width-down/128?cb=20210413210802")
+    .setFooter("Look for another? try ^list") ;
+  message.channel.send(embed);
+
+    }       
+    
+    module.exports.help = {
+        name: `hakushinring`,
+        aliases: ["haku", "hakushin", "ring"]
+    }
