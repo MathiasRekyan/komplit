9261d67363d168e1230f6546cf285150e539a99f
MathiasRekyan <79878805+MathiasRekyan@users.noreply.github.com>
2021-07-28 17:40:24 +0700

    Create ameno.js

---
+++ commands/weapon sword/ameno.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (Client, message, args, prefix) => {
+    if(!message.content.startsWith(prefix)) return;
+
+    const embed = new Discord.MessageEmbed()
+    .addField("Amenoma Kageuchi", "⧫ Type: " + "Sword" + "\r\n ⧫ Rarity: " + "⭐⭐⭐⭐" + "\r\n ⧫ Location: " + "Craft" , true)
+    .addField("\u200B", "⧫ ATK: " + "41 (max 454)" + "\r\n ⧫ 2nd Stat: " + "Energy Recharge 10% (max 45.9%)", true)
+    .addField("Passive Ability: Iwakura Succession", "After casting an Elemental Skill, gain 1 Succession Seed. This effect can be triggered once every 5s. The Succession Seed lasts for 30s. Up to 3 Succession Seeds may exist simultaneously. After using an Elemental Burst, all Succession Seeds are consumed and after 2s, the character regenerates 6~12 Energy for each seed consumed.")
+    .setImage("https://rerollcdn.com/GENSHIN/Weapon/NEW/Amenoma_Kageuchi.png", 2, 2)
+    .setThumbnail("https://static.wikia.nocookie.net/gensin-impact/images/8/81/Icon_Sword.png/revision/latest/scale-to-width-down/128?cb=20210413210800")
+    .setFooter("Look for another? try ^list") ;
+  message.channel.send(embed);
+
+    }       
+    
+    module.exports.help = {
+        name: `amenomakageuchi`,
+        aliases: ["amenoma" , "amen" , "noma" ]
+    }
