a8aba37b122b115d1a06b1f6e74754de06739507
MathiasRekyan <79878805+MathiasRekyan@users.noreply.github.com>
2021-08-17 21:36:09 +0700

    Update pulse.js

--- commands/weapon bow/pulse.js
+++ commands/weapon bow/pulse.js
@@ -8,7 +8,7 @@ module.exports.run = async (Client, message, args, prefix) => {
     .addField("\u200B","⧫ Type: " + "Bow" + "\r\n ⧫ Rarity: " + "⭐⭐⭐⭐⭐" + "\r\n ⧫ Location: " + "Gacha" , true)
     .addField("\u200B", "⧫ ATK: " + "46 (max 608)" + "\r\n ⧫ 2nd Stat: " + "CRIT DMG 14.4% (Maks 66.2%)", true)
     .addField("Passive Ability: Rule By Thunder", "Increases ATK by 20~40% and grants the might of the Thunder Emblem. At stack levels 1/2/3, the Thunder Emblem increases Normal Attack DMG by 12/24/40~24/48/80%.")
-    ..addField("Continuation","Increases ATK by 20~40% and grants the might of the Thunder Emblem. At stack levels 1/2/3, the Thunder Emblem increases Normal Attack DMG by 12/24/40~24/48/80%.")
+    .addField("Continuation","Increases ATK by 20~40% and grants the might of the Thunder Emblem. At stack levels 1/2/3, the Thunder Emblem increases Normal Attack DMG by 12/24/40~24/48/80%.")
     .setImage("https://rerollcdn.com/GENSHIN/Weapon/NEW/Amos'_Bow.png", 2, 2) 
     .setThumbnail("https://static.wikia.nocookie.net/gensin-impact/images/8/81/Icon_Bow.png/revision/latest/scale-to-width-down/128?cb=20210413210801")
     .setFooter("Look for another? try ^list") ;
