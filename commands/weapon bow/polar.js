890f2217e596fe8a529bfee0f6f1a4d8cd0b774b
MathiasRekyan <79878805+MathiasRekyan@users.noreply.github.com>
2021-10-22 10:00:54 +0700

    Create polar.js

---
+++ commands/weapon bow/polar.js
@@ -0,0 +1,22 @@
+   
+const Discord = require('discord.js');
+
+module.exports.run = async (Client, message, args, prefix) => {
+    if(!message.content.startsWith(prefix)) return;
+
+    const embed = new Discord.MessageEmbed()
+    .setTitle("Polar Star")
+    .addField("\u200B","⧫ Type: " + "Bow" + "\r\n ⧫ Rarity: " + "⭐⭐⭐⭐⭐" + "\r\n ⧫ Location: " + "Gacha" , true)
+    .addField("\u200B", "⧫ ATK: " + "46 (max 608)" + "\r\n ⧫ 2nd Stat: " + "Crit Rate 7.2% (Maks 33.1%)", true)
+    .addField("Elemental Skill and Elemental Burst DMG increased by 12%. After a Normal Attack, Charged Attack, Elemental Skill or Elemental Burst hits an opponent, 1 stack of Ashen Nightstar will be gained for 12s. When 1/2/3/4 stacks of Ashen Nightstar are present, ATK is increased by 10/20/30/48%. The stack of Ashen Nightstar created by the Normal Attack, Charged Attack, Elemental Skill or Elemental Burst will be counted independently of the others.")
+    .setImage("https://static.wikia.nocookie.net/gensin-impact/images/4/44/Weapon_Polar_Star.png/revision/latest/scale-to-width-down/256?cb=20211013042349", 2, 2) 
+    .setThumbnail("https://static.wikia.nocookie.net/gensin-impact/images/8/81/Icon_Bow.png/revision/latest/scale-to-width-down/128?cb=20210413210801")
+    .setFooter("Look for another? try ^list") ;
+  message.channel.send(embed);
+
+    }       
+    
+    module.exports.help = {
+        name: `polarstar`, 
+        aliases: ["polar", "star"]
+    }
